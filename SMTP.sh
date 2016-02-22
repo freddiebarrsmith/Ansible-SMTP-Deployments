6be7900f068cb70979a9c74fd42cd4cd48bf9e01
root <fredbarrsmith@gmail.com>
2016-02-22 21:15:37 +0000

    SMTP

---
+++ SMTP.sh
@@ -0,0 +1,12 @@
+echo "Type the domain name followed by [ENTER]:"
+
+read domain
+
+hostname="$1"
+apt-get -y update
+debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
+debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+apt-get install -y postfix
+sed -i '31s/.*/myhostname = $domain/' /etc/postfix/main.cf
+postmap /etc/postfix/virtual
+service postfix restart
