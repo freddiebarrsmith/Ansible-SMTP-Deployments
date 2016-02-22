0b70154669d1c688acbe5f1f52924de4ba65ff0e
root <fredbarrsmith@gmail.com>
2016-02-22 21:27:29 +0000

    fixed the blasted preconfig

--- SMTP.sh
+++ SMTP.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 echo "Type the domain name followed by [ENTER]:"
 
 read domain
 
-hostname="$1"
 apt-get -y update
 debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
@@ -10,3 +10,6 @@ apt-get install -y postfix
 sed -i '31s/.*/myhostname = $domain/' /etc/postfix/main.cf
 postmap /etc/postfix/virtual
 service postfix restart
+
+
+
