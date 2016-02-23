913f2891a51d0a2ad66cef4245288a1e5c39f06b
root <fredbarrsmith@gmail.com>
2016-02-23 21:17:10 +0000

    slight spelling mistake, still doesn't work tho

--- SMTP.sh
+++ SMTP.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 echo "Type the domain name followed by [ENTER]:"
-
 read domain
-
 apt-get -y update
-debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
+debconf-set-selections <<< "postfix postfix/mailname string archangelsystems.co.uk"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 apt-get install -y postfix
-sed -i '31s/.*/myhostname = $domain/' /etc/postfix/main.cf
-postmap /etc/postfix/virtual
+sed -i '31s/.*/myhostname = archangelsystems.co.uk/' /etc/postfix/main.cf
 service postfix restart
-
-
 
